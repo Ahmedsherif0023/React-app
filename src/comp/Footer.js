20e0411e92c5d51d95e12394a0f8bebe6badcbfd
Shefo <ahmedsherifoo23@gmail.com>
2023-05-31 15:22:00 -0400

    first commit

---
+++ src/comp/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="myFooter">
+      <footer className="shefo">
+        Designed and developed by Shefo.com
+        <span>🧡</span>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
